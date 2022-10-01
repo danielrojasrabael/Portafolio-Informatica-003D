6af36f031a75f5349657d8001f986dfb22e000e4
danielrojasrabael <daa.rojasr@duocuc.cl>
2022-10-01 00:37:41 -0300

    4 pruebas añadidas
    hay que arreglar weaitas eso si

--- SSAP_Web/SSAP/test.py
+++ SSAP_Web/SSAP/test.py
@@ -1,10 +1,11 @@
 from django.test import SimpleTestCase
 from SSAP.models import *
 
-conexion = cx_Oracle.connect(user="SSAP_TEST", password="123456", dsn="localhost:1522/ORCL1")
+conexion = cx_Oracle.connect(user="SSAP_TEST", password="123456", dsn="localhost:1521/XE")
 
 # -- CU 1 --
 
+    # -- 3 --
 class CrearClientePrueba(SimpleTestCase):
     def setUp(self):
         #usuario = Usuario(contraseña = "1234", tipo = "CLIENTE", id_comuna = 1, direccion = "Calle Falsa")
@@ -16,6 +17,7 @@ class CrearClientePrueba(SimpleTestCase):
         cliente= Cliente.filtro_id(1, conn=conexion)
         self.assertEquals(cliente.rut, "11.111.111-1")
 
+    # -- 2 --
 class DeshabilitarUsuarioPrueba(SimpleTestCase):
 
     def test_deshabilitar_cliente(self):
@@ -24,5 +26,43 @@ class DeshabilitarUsuarioPrueba(SimpleTestCase):
         cliente1=Usuario.filtro_id(1, conn=conexion)
         self.assertEquals(cliente1.estado, 0)
 
+    # -- 4 --
+class ModificarUsuarioPrueba(SimpleTestCase):
+
+    def test_modificar_cliente(self):
+        cliente1 = Cliente.filtro_id(1, conn=conexion)
+        cliente1.cant_trabajadores = 10
+        cliente1.actualizar(conn=conexion)
+        self.assertEquals(cliente1.cant_trabajadores, 10)
+
 # -- CU 2 --
 
+    # -- 6 --
+class CrearProfesionalPrueba(SimpleTestCase):
+    def setUp(self):
+        #usuario = Usuario(contraseña = "1234", tipo = "CLIENTE", id_comuna = 1, direccion = "Calle Falsa")
+        #usuario.guardar(conn=conexion)
+        profesional = Profesional(id_usuario=21,rut="22.222.222-2",nombre="Vicente")
+        profesional.guardar(conn=conexion)
+
+    def test_creacion_profesional(self):
+        profesional= Profesional.filtro_id(21, conn=conexion)
+        self.assertEquals(profesional.rut, "22.222.222-2")
+
+    # -- 5 --
+class DeshabilitarProfesionalPrueba(SimpleTestCase):
+
+    def test_deshabilitar_profesional(self):
+        profesional=Usuario.filtro_id(21, conn=conexion)
+        profesional.deshabilitar(conn=conexion)
+        profesional1=Usuario.filtro_id(21, conn=conexion)
+        self.assertEquals(profesional1.estado, 0)
+
+    # -- 7 --
+class ModificarProfesionalPrueba(SimpleTestCase):
+
+    def test_modificar_profesional(self):
+        profesional = Profesional.filtro_id(21, conn=conexion)
+        profesional.nombre = "Enzo"
+        profesional.actualizar(conn=conexion)
+        self.assertEquals(profesional.nombre, "Enzo")
